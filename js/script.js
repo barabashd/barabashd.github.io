b99de2b8c3d5724786e0f909ef822e961220f8c0
Dmytro Barabash <barabashdmyto@gmail.com>
2019-02-04 14:02:38 +0200

    remove last design

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
 
-    $preloader = $('.preloader'),
-    $loader = $preloader.find('.preloader');
-    $loader.fadeOut();
-    $preloader.delay(1000).fadeOut(1500);
-
-    $(".navigation > li > a").click(function(event) {
+    $(".navigation > li").on("click", "a", function(event) {
         event.preventDefault();
         var id = $(this).attr('href'),
             top = $(id).offset().top - 60;
@@ -16,19 +11,25 @@ $(document).ready(function() {
 
     $('.intro_block .about_me').addClass('intro_img_visible');
 
+
     if (window.matchMedia('(max-width: 991px)').matches) {
         $('.mobile_menu').click(function() {
             $('header nav').toggleClass('mobile_menu_active');
             if ($('header nav').hasClass('mobile_menu_active')) {
+                /*$('body, html').css('height', '100%');
+                $('body, html').css('margin', '0');
+                $('body, html').css('overflow', 'hidden');*/
                 $(".navigation > li").on("click", "a", function(){
                     $('header nav').removeClass('mobile_menu_active');
                 });
             } else {
             	$('header nav').removeClass('mobile_menu_active');
+                /*$('body, html').css('overflow', 'visible');*/
             }
         });
     } else {
         $('header nav').removeClass('mobile_menu_active');
+        /*$('body, html').css('overflow', 'visible');*/
 	}
 
 
@@ -37,6 +38,7 @@ $(document).ready(function() {
 
         } else {
         	$('header nav').removeClass('mobile_menu_active');
+            /*$('body, html').css('overflow', 'visible');*/
         }
     });
 
@@ -45,8 +47,9 @@ $(document).ready(function() {
         slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 2000,
         arrows: false,
+        centerMode: true,
         responsive: [{
                 breakpoint: 1200,
                 settings: {
@@ -64,33 +67,5 @@ $(document).ready(function() {
 
 
 
-    /*blog*/
-    $(".all_post_block > .wrapper > a").addClass('active_post'); 
-    var min_page_height = $(window).height() - $('#contact').height();
-    $('.all_post_block').css('min-height', min_page_height);
-    $(window).resize(function(){
-        min_page_height = $(window).height() - $('#contact').height();
-        $('.all_post_block').css('min-height', min_page_height);
-    });
-    $('.filter_post > ul > li').click(function(){
 
-        $('.filter_post > ul > li').removeClass('active_tabs');
-        $(this).addClass('active_tabs');
-        
-        if( $(this).hasClass('all') ){
-            $(".all_post_block > .wrapper > a").addClass('active_post');  
-        } 
-        if( $(this).hasClass('travel') ){
-            $(".all_post_block > .wrapper > a").removeClass('active_post'); 
-            $(".all_post_block > .wrapper > a[data_tag='travel']").addClass('active_post');  
-        } 
-        if( $(this).hasClass('code') ){
-            $(".all_post_block > .wrapper > a").removeClass('active_post'); 
-            $(".all_post_block > .wrapper > a[data_tag='code']").addClass('active_post');  
-        } 
-        if( $(this).hasClass('other') ){
-            $(".all_post_block > .wrapper > a").removeClass('active_post'); 
-            $(".all_post_block > .wrapper > a[data_tag='other']").addClass('active_post');  
-        } 
-    });
 });
